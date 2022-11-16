db8907d7aa7ba9ee8c43495b65ce67bd67e5c1e1
rebahama <wiri@hotmail.se>
2022-11-16 05:04:58 +0000

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React  from 'react';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import HomePage from './webpage/HomePage';
 
 function App() {
   return (
-    <div className="App">
+    
+    <div className={styles.App}>
      <NavBar/>
+     <Routes>
+     <Route exact path="/hello"  element={<HomePage />} />
+     </Routes>
+     
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,22 @@
 import React from 'react'
+import {
+    NavLink
+  } from 'react-router-dom';
 import { Container,Navbar,Nav } from 'react-bootstrap'
 
 const NavBar = () => {
   return (
+    
+    <Navbar bg="dark" variant="dark" expand="lg">
     <Container>
-    <Navbar bg="primary" variant="dark">
-    <Container>
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <NavLink to="/">Navbar</NavLink>
       <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
+        <NavLink to="/hello"> Home</NavLink>
+  
       </Nav>
     </Container>
   </Navbar>
-  </Container>
+  
 
   )
 }
