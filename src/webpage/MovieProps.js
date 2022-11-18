81d5ba28e52bd1083fe6a89649ceedd5efb90bab
rebahama <wiri@hotmail.se>
2022-11-18 04:44:19 +0000

    add card images

--- src/webpage/MovieProps.js
+++ src/webpage/MovieProps.js
@@ -1,18 +1,35 @@
 import React from 'react'
+import styles from '../styles/MoviePage.module.css';
+import { Card, Col, Container, Row } from 'react-bootstrap'
 
+
+// Movie details: https://www.omdbapi.com/?apikey=41059430&i=tt1285016&type=movie
 const MovieProps = (props) => {
-    const {imdbID,Title, Year}=props
-  return (
-    <div>
-        
-        <p>{Title} {Year} <a href={` https://www.imdb.com/title/${imdbID}`} target="_blanc">Read more</a> </p>
-        
-      
-        
-    
+    const { imdbID, Title, Year, Poster } = props
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col md={4} className="offset-md-5">
+                        <Card className={styles.CardStyles}>
+                            <Card.Img variant="top" src={Poster} />
+                            <Card.Body>
+                                <Card.Title>{Title}</Card.Title>
+                                <Card.Text>
+                                    {Year}
 
-    </div>
-  )
+                                    <a href={` https://www.imdb.com/title/${imdbID}`} target="_blanc">Read more</a>
+                                </Card.Text>
+
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+
+
+        </div>
+    )
 }
 
 export default MovieProps
--- src/webpage/Movies.js
+++ src/webpage/Movies.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState } from 'react'
 import axios from 'axios'
 import styles from '../styles/MoviePage.module.css';
-import { Container, Form} from 'react-bootstrap';
+import { Card, Container, Form, Row} from 'react-bootstrap';
 import SpinnerBar from '../components/SpinnerBar';
 import MovieProps from './MovieProps';
 
@@ -10,39 +10,36 @@ import MovieProps from './MovieProps';
 const Movies = () => {
 
   const axiosReq = axios.create();
-  const [loaded,setLoaded] = useState(false)
+  const [loaded,setLoaded] = useState(false);
   const [query, setQuery] = useState("revenant");
   const baseUrl = `https://www.omdbapi.com/?apikey=41059430&s=${query}&type=movie`
   const [movies, setMovies] = useState([]);
-  
- 
 
   useEffect(() => {
     const handleData = async () => {
       try {
-        const {data} = await axiosReq.get(baseUrl)
-        console.log(data.Search)
-        setMovies(data.Search)
-        setLoaded(true)
+        const {data} = await axiosReq.get(baseUrl);
+        console.log(data.Search);
+        setMovies(data.Search);
+        setLoaded(true);
       }
       
       catch (err) {
         console.log(err)
       }
     };
-    setLoaded(false)
+    setLoaded(false);
 
     const timer = setTimeout(()=>{
       handleData();
 
-
     }, 2000)
 
     return ()=>{
-      clearTimeout(timer)
+      clearTimeout(timer);
     }
         
-  }, [query])
+  }, [query]);
 
     
 
@@ -52,17 +49,13 @@ const Movies = () => {
  <Form onSubmit={(event) => event.preventDefault()}>
   <Container>
  <Form.Control type="text" className={styles.SearchBar}placeholder= "Search a review" value={query} onChange={(event) => setQuery(event.target.value)}  />
- 
+
  </Container>     
           </Form>
-
-
-    
+          
      {loaded ? <> {movies && movies.map((movie)=>{
-      return <MovieProps key={movie.imdbID} {...movie}/>})}</>
-      
-      :<SpinnerBar/>} 
-
+      return  <MovieProps key={movie.imdbID} {...movie} />} )}</>
+      :<SpinnerBar/>}
     </div>
   )
 }
