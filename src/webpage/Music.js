2b4e55271557fe774b726c42a3a117defc40f2aa
rebahama <wiri@hotmail.se>
2022-11-24 04:13:28 +0000

    add input fields

--- src/webpage/Music.js
+++ src/webpage/Music.js
@@ -8,42 +8,37 @@ const Music = () => {
 
   const [music, SetMusic] = useState([]);
   const [lyric, setLyrics] = useState("");
-  useEffect(() => {
 
-    const options = {
-      method: 'GET',
-      url: 'https://shazam.p.rapidapi.com/search',
-      params: { term: lyric, locale: 'en-US', offset: '0', limit: '5' },
-      headers: {
-        'X-RapidAPI-Key': '77677621eamshcb48954032d980dp1e8fc7jsn8735e2ae11b4',
-        'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
-      }
-    };
 
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      axios.request(options).then(function (response) {
-        console.log(response.data.tracks.hits);
-        SetMusic(response.data.tracks.hits)
-        console.log(music)
-
-      }).catch(function (error) {
-
-        console.error(error);
-      });
-
+  const options = {
+    method: 'GET',
+    url: 'https://shazam.p.rapidapi.com/search',
+    params: { term: lyric, locale: 'en-US', offset: '0', limit: '5' },
+    headers: {
+      'X-RapidAPI-Key': '77677621eamshcb48954032d980dp1e8fc7jsn8735e2ae11b4',
+      'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
     }
+  };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.request(options).then(function (response) {
+      console.log(response.data.tracks.hits);
+      SetMusic(response.data.tracks.hits)
+      console.log(music)
 
+    }).catch(function (error) {
 
+      console.error(error);
+    });
 
+  }
 
-  }, [])
 
   return (
 
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={lyric}
