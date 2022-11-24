73522163902c88771ca1d97cb213f7755ea72d45
rebahama <wiri@hotmail.se>
2022-11-24 04:06:50 +0000

    add input field

--- src/webpage/Music.js
+++ src/webpage/Music.js
@@ -7,27 +7,33 @@ const Music = () => {
 
 
   const [music, SetMusic] = useState([]);
+  const [lyric, setLyrics] = useState("");
   useEffect(() => {
 
     const options = {
       method: 'GET',
       url: 'https://shazam.p.rapidapi.com/search',
-      params: {term: 'billie jean', locale: 'en-US', offset: '0', limit: '5'},
+      params: { term: lyric, locale: 'en-US', offset: '0', limit: '5' },
       headers: {
         'X-RapidAPI-Key': '77677621eamshcb48954032d980dp1e8fc7jsn8735e2ae11b4',
         'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
       }
     };
 
-    axios.request(options).then(function (response) {
-      console.log(response.data.tracks.hits);
-      SetMusic(response.data.tracks.hits)
-      console.log(music)
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      axios.request(options).then(function (response) {
+        console.log(response.data.tracks.hits);
+        SetMusic(response.data.tracks.hits)
+        console.log(music)
 
-    }).catch(function (error) {
+      }).catch(function (error) {
 
-      console.error(error);
-    });
+        console.error(error);
+      });
+
+    }
+
 
 
 
@@ -37,22 +43,29 @@ const Music = () => {
   return (
 
     <div>
+      <form>
+        <input
+          type="text"
+          value={lyric}
+          onChange={(e) => setLyrics(e.target.value)}
+        />
+      </form>
+      {music.map((musics) => {
 
-    {music.map((musics)=>{
+        return <div key={musics.track.key}>
 
-      return <div key={musics.key}> 
-      
-      
-      <p> song name: {musics.snippet}</p>
 
-      <p> Artis: {musics.track.subtitle}</p>
-      <img src={musics.track.images.background} alt="artist avatar"></img>
-      
-      </div>
+          <p> song name: {musics.track.title}</p>
 
-    })}
+          <p> Artist: {musics.track.subtitle}</p>
+          <img src={musics.track.images.background} alt="artist avatar"></img>
+          <a href={musics.track.url} target='_blank'> Click here to hear song </a>
 
-  </div>
+        </div>
+
+      })}
+
+    </div>
   )
 }
 
