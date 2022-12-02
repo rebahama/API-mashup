97ebcd3b85116b662a23849058263b0f138edec6
rebahama <wiri@hotmail.se>
2022-12-02 04:08:49 +0000

    style movie page

--- src/webpage/MovieProps.js
+++ src/webpage/MovieProps.js
@@ -8,20 +8,28 @@ import { Link } from 'react-router-dom';
 // Movie details: https://www.omdbapi.com/?apikey=41059430&i=tt1285016&type=movie
 const MovieProps = (props) => {
 
-    const { imdbID, Title, Year, Poster } = props;
+    const { imdbID, Title, Year, Poster,Type } = props;
 
     if (Poster === "N/A") {
 
         return <div>
             <Card className={styles.CardStyles}>
+                
                 <video variant="top" src={image} autoPlay loop muted playsInline className={styles.VideoStyle}> </video>
                 <Card.Body>
-                    <Card.Title >{Title} {Year}</Card.Title>
+                    <hr/>
+                <Card.Title>{Title} </Card.Title>
+                <hr/>
+                    <p> Year: {Year}</p>
+                    <hr/>
+                    <p> Type: {Type}</p>
+                    <hr/>
                     <Card.Text>
-                        <a href={` https://www.imdb.com/title/${imdbID}`} target="_blanc">Read more on imdb</a>
+                        
                     </Card.Text>
+                    <Link to={`/moviedetails/${imdbID}`} className={styles.viewMore}> View more</Link>
                 </Card.Body>
-                <Link to={`/moviedetails/${imdbID}`}> View more</Link>
+               
                 
             </Card>
         </div>
@@ -30,13 +38,21 @@ const MovieProps = (props) => {
     else if (Poster !== "N/A") {
         return <div>
             <Card className={styles.CardStyles}>
-                <Card.Img variant="top" src={Poster}  ></Card.Img>
+                
+                <Card.Img variant="top" src={Poster} className={styles.cardImage} ></Card.Img>
                 <Card.Body>
-                    <Card.Title>{Title} {Year}</Card.Title>
+                    <Card.Title>{Title} </Card.Title>
+                    <hr/>
+                    <p> Year: {Year}</p>
+                    <hr/>
+                    <p> Type: {Type}</p>
+                    <hr/>
                     <Card.Text>
+                    <Link to={`/moviedetails/${imdbID}`} className={styles.viewMore}> View more</Link>
                     </Card.Text>
+                    
                 </Card.Body>
-                <Link to={`/moviedetails/${imdbID}`} className={styles.viewMore}> View more</Link>
+                
             </Card>
         </div>
     }
