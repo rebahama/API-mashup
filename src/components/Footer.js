011728cd35483be5fa36166ad8c1671a1839a9f9
rebahama <wiri@hotmail.se>
2022-12-09 01:29:47 +0000

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,43 @@
-import React from 'react'
-import styles from '../styles/FooterPage.module.css'
+import React from "react";
+import styles from "../styles/FooterPage.module.css";
 
 const Footer = () => {
+
   return (
     <div className={styles.Footer}>
+      <ul className={styles.FooterLinks}>
+        <li className="list-inline-item">
+          <a
+            href="https://sv-se.facebook.com/"
+            target="_blank"
+            rel=" noopener noreferrer"
+          >
+            Facebook
+          </a>
+        </li>
+        <li className="list-inline-item">
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+            rel=" noopener noreferrer"
+          >
+            Instagram
+          </a>
+        </li>
+        <li className="list-inline-item">
+          <a
+            href="https://twitter.com/?lang=sv"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+        </li>
+        <p> Created by Rebas Ahmad</p>
+      </ul>
+    </div>
+  );
 
-                  <ul className={styles.FooterLinks}>
-                    
-                        <li className="list-inline-item"><a href="https://sv-se.facebook.com/" target="_blank"rel=" noopener noreferrer"> Facebook </a></li>
-                        <li className="list-inline-item"><a href="https://www.instagram.com/" target="_blank" rel=" noopener noreferrer"> Instagram</a></li>
-                        <li className="list-inline-item"><a href="https://twitter.com/?lang=sv" target="_blank" rel="noopener noreferrer"> Twitter</a></li>
-                        <p> Created by Rebas Ahmad</p>
-                  </ul>
-                  
-            </div>
+};
 
-  )
-}
-
-export default Footer
+export default Footer;
