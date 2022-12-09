1ecea3fc8800ccbfac3afaaad1367a8e93217e98
rebahama <wiri@hotmail.se>
2022-12-09 01:33:01 +0000

    fix homepage

--- src/webpage/HomePage.js
+++ src/webpage/HomePage.js
@@ -1,45 +1,47 @@
-import React from 'react'
-import styles from '../styles/HomePage.module.css'
-import image from '../assets/movie-cut.mp4';
-import imagemusic from '../assets/music.mp4'
-import {
-  NavLink
-} from 'react-router-dom';
-import { Col, Container, Row } from 'react-bootstrap';
-import Carousel from 'react-bootstrap/Carousel';
-
-
+import React from "react";
+import styles from "../styles/HomePage.module.css";
+import image from "../assets/movie-cut.mp4";
+import imagemusic from "../assets/music.mp4";
+import { NavLink } from "react-router-dom";
+import { Col, Container, Row } from "react-bootstrap";
+import Carousel from "react-bootstrap/Carousel";
 
 const HomePage = () => {
+
   return (
     <Container fluid>
       <Row>
-        <Col md={12} className={styles.HeroImage}>
-        </Col>
+        <Col md={12} className={styles.HeroImage}></Col>
       </Row>
       <h1> Movie and music searcher</h1>
       <hr className={styles.hrLine} />
-      <h2> Welcome on this page you can either search and find info about a movie or try our lyric finder that finds a song based on a couple of sentences</h2>
+      <h2>
+        Welcome on this page you can either search and find info about a movie
+        or try our lyric finder that finds a song based on a couple of sentences
+      </h2>
       <Carousel variant="dark">
         <Carousel.Item>
-          <video variant="top" src={image} autoPlay loop muted playsInline> </video>
+          <video variant="top" src={image} autoPlay loop muted playsInline>
+          </video>
           <Carousel.Caption>
-
-            <NavLink to="/movies" className={styles.MovieBtn} >Movies </NavLink>
+            <NavLink to="/movies" className={styles.MovieBtn}>
+              Movies
+            </NavLink>
           </Carousel.Caption>
         </Carousel.Item>
-
         <Carousel.Item>
-          <video variant="top" src={imagemusic} autoPlay loop muted playsInline> </video>
+          <video variant="top" src={imagemusic} autoPlay loop muted playsInline>
+          </video>
           <Carousel.Caption>
-
-            <NavLink to="/music" className={styles.MusicBtn} >Lyricfinder </NavLink>
+            <NavLink to="/music" className={styles.MusicBtn}>
+              Lyricfinder
+            </NavLink>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-
     </Container>
-  )
+  );
+
 };
 
-export default HomePage
+export default HomePage;
